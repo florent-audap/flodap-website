b8997b520c91d3b8b309841d3fd8ca04f4c8e5d3
Florent Audap <florent.audap@student-cs.fr>
2026-01-23 20:53:27 +0100

    [start] first changes to start with the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import siteLogo from '/Logo-FA.png'
 import './App.css'
 
 function App() {
@@ -10,24 +9,16 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={siteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Site web de Florent AUDAP</h1>
       <div className="card">
+        <h3>Comptez jusqu'à autant que vous voulez</h3>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
