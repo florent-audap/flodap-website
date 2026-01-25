1adec455de987c569034b0dd681206970801b922
Florent Audap <florent.audap@student-cs.fr>
2026-01-25 22:16:15 +0100

    V1 Contact Form with Resend

---
+++ src/components/contactForm.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { useContactForm } from "../hooks/useContactForm";
+
+export function ContactForm() {
+  const { submit, loading, success, error } = useContactForm();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+    company: ""
+  });
+
+  useEffect(() => {
+    if (success) {
+      setForm({ name: "", email: "", message: "", company: "" });
+    }
+  }, [success]);
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        submit(form);
+      }}
+      className="space-y-4 p-10 flex flex-col items-center"
+    >
+      <input
+        className="input input-bordered"
+        placeholder="Name"
+        value={form.name}
+        onChange={e => setForm({ ...form, name: e.target.value })}
+      />
+
+      <input
+        required
+        type="email"
+        className="input input-bordered"
+        placeholder="Email"
+        value={form.email}
+        onChange={e => setForm({ ...form, email: e.target.value })}
+      />
+
+      <textarea
+        required
+        className="textarea textarea-bordered"
+        placeholder="Message"
+        value={form.message}
+        onChange={e => setForm({ ...form, message: e.target.value })}
+      />
+
+      {/* Honeypot */}
+      <input
+        type="text"
+        name="Company"
+        className="hidden"
+        value={form.company}
+        onChange={e => setForm({ ...form, company: e.target.value })}
+      />
+
+      <button className="btn btn-primary" disabled={loading}>
+        {loading ? "Sending..." : "Send Message"}
+      </button>
+
+      {success && <p className="text-success">Message succesfully sent ✅</p>}
+      {error && <p className="text-error">{error}</p>}
+    </form>
+  );
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,5 @@
+import { ContactForm } from "../components/contactForm";
+
 export default function ContactPage() {
   return (
     <>
@@ -7,15 +9,13 @@ export default function ContactPage() {
         like to talk with me about any subject you’re interested in, feel free
         to contact me !
       </p>
-      <div className="card bg-base-200 shadow-xl border border-gray-600 w-full max-w-md mx-auto mt-16">
-        <div className="card-body items-center text-center">
-          <h3 className="card-title">Under construction</h3>
-          <img
-            src="https://media.tenor.com/lMmZyvvNzCMAAAAj/shoot-shooting.gif"
-            alt="Construction GIF"
-            className="rounded-xl mt-4"
-          />
-        </div>
+      <div className="card place-self-center bg-base-200 shadow-xl border border-gray-600 max-w-2/3 min-w-2xl mt-16">
+        <h2 className="text-center m-3 text-blue-600">Contact Form</h2>
+        <ContactForm/>
+      </div>
+      <div className="text-center mt-8 mb-10">
+        <p className="mb-2">If you have some trouble with the contact form, you can send an email to</p>
+        <a href="mailto:contact@florentaudap.com" className="link link-primary font-semibold">contact@florentaudap.com</a>
       </div>
     </>
   );
