46183c4a4ac9d7aca214943c4364207e17d7e6fb
Florent Audap <florent.audap@student-cs.fr>
2026-01-28 13:00:39 +0100

    [About] Initial version with incomplete information

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="footer footer-horizontal footer-center text-white bg-primary rounded gap-8 p-5">
+    <footer className="footer footer-horizontal footer-center text-white bg-primary rounded-t gap-8 p-5">
       <nav>
         <div className="grid grid-flow-col gap-4">
           <a
